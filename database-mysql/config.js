8cc68e313bc26e8cbcf109afdebbbfcfdc24c9de
Himanshu Gaur <himanshugaur2610@gmail.com>
2018-11-16 01:42:30 -0600

    anged password

--- database-mysql/config.js
+++ database-mysql/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: 'overview.c3y0pvsiumcx.us-east-1.rds.amazonaws.com',
   user: 'himanshugaur2610',
-  password: 'Astringent1',
+  password: 'password',
   database: 'overview'
 };
